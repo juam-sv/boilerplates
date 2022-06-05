d75421a73c3054c058fb5114e3411782f04eecd2
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2022-06-05 23:13:41 +0000

    Update on libvirt project for terraform

--- Terraform/libvirt/main.tf
+++ Terraform/libvirt/main.tf
@@ -10,6 +10,30 @@ provider "libvirt" {
     uri = "qemu:///system"
 }
 
+resource "libvirt_volume" "centos7-qcow2" {
+    name = "centos7-qcow2"
+    pool = "default"
+    source = "/var/lib/libvirt/images/CentOS-7-x86_64-GenericCloud.qcow2c"
+    format = "qcow2"
+    # capacity = "10"
+    # provider = "libvirt"
+}
+
 resource "libvirt_domain" "terraform_test" {
     name = "terraform_test"
+    memory = 2048
+    vcpu = 2
+
+    disk {
+        volume_id = libvirt_volume.centos7-qcow2.id
+    }
+
+    network_interface {
+        network_name = "default"
+    }
+}
+
+output "IPS" {
+    # value = libvirt_domain.terraform_test.ip_addresses
+    value = libvirt_domain.terraform_test.*.network_interface.0.addresses
 }
