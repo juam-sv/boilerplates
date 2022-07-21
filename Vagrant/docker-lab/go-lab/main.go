b56008277c6b04889f16fe2fbc4e38688a79a37c
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2022-07-21 16:51:32 -0300

    Add of go script test

---
+++ Vagrant/docker-lab/go-lab/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("<h1>AWEKEN MY MASTERS!!!!</h1>"))
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
