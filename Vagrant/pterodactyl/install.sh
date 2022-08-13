93cae7cf6629f58b43cfb88e48048c8b7a42adf5
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2022-08-13 11:56:19 -0300

    Update on pterodactyl

---
+++ Vagrant/pterodactyl/install.sh
@@ -0,0 +1,44 @@
+curl -sSL https://get.docker.com/ | CHANNEL=stable bash
+systemctl enable --now docker
+
+mkdir -p /etc/pterodactyl
+curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
+chmod u+x /usr/local/bin/wings
+
+# Add "add-apt-repository" command
+apt -y install software-properties-common curl apt-transport-https ca-certificates gnupg
+
+# Add additional repositories for PHP, Redis, and MariaDB
+LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
+add-apt-repository ppa:redislabs/redis -y
+
+# MariaDB repo setup script can be skipped on Ubuntu 22.04
+curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
+
+# Update repositories list
+apt update
+
+# Add universe repository if you are on Ubuntu 18.04
+apt-add-repository universe
+
+# Install Dependencies
+apt -y install php8.1 php8.1-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip} mariadb-server nginx tar unzip git redis-server
+
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+
+
+mkdir -p /var/www/pterodactyl
+cd /var/www/pterodactyl
+
+curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz
+tar -xzvf panel.tar.gz
+chmod -R 755 storage/* bootstrap/cache/
+
+#------------------------MYSQL BLOCK------------------------#
+mysql -u root -p
+
+# Remember to change 'yourPassword' below to be a unique password
+CREATE USER 'pterodactyl'@'127.0.0.1' IDENTIFIED BY '7rz*9M!sY$&oYkmk$w8z853!H9a%$U#&';
+CREATE DATABASE panel;
+GRANT ALL PRIVILEGES ON panel.* TO 'pterodactyl'@'127.0.0.1' WITH GRANT OPTION;
+exit
