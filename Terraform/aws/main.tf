941826d94c2c63487e357c3bb7ca006cba05fb39
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2022-09-01 22:40:16 -0300

    init of aws terraform lab

---
+++ Terraform/aws/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-lab-madra"
+    key    = "terraform-test.tfstate"
+    region = "us-east-2"
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
