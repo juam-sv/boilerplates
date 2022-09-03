0caa9e6945e78cecc796df2f92f6d7c68a99efe6
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2022-09-03 18:54:50 -0300

    update on labs of terraform

--- Terraform/aws/main.tf
+++ Terraform/aws/main.tf
@@ -18,15 +18,54 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
+  # alias  = "apelido-name"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+resource "aws_instance" "zawarudo" {
+  ami           = var.image
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.freeword_snet.id
+  # provider = aws.apelido-name
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "THE_WORLD"
   }
 }
 
+# VPC SECTION
+
+resource "aws_vpc" "vpc_dreams" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "freeword_snet" {
+  vpc_id     = aws_vpc.vpc_dreams.id
+  cidr_block = "10.0.1.0/24"
+  # availability_zone = "us-east-1"
+}
+
+resource "aws_subnet" "prision_snet" {
+  vpc_id     = aws_vpc.vpc_dreams.id
+  cidr_block = "10.0.2.0/24"
+}
+
+# RDS SECTION
+/**
+resouce "aws_db_instance" "db" {
+  allocated_storage = 20
+  storage_type = "gp2"
+  engine = "mysql"
+  engine_version = "5.7"
+  instance_class = "db.t2.micro"
+  name = "mydb"
+  username = "admin"
+  password = "password"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot = true
+  publicly_accessible = true
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  db_subnet_group_name = aws_db_subnet_group.db_sng.name
+}
+**/
