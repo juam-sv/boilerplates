3739f1c7bc1ff91b550ebe9dcfce456eb6ca6d4b
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2022-10-10 22:07:15 -0300

    change version and add of output

--- Azure/hello-world/instance.tf
+++ Azure/hello-world/instance.tf
@@ -22,7 +22,7 @@ resource "azurerm_linux_virtual_machine" "vm_linux" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "20.04-LTS"
+    sku       = "16.04-LTS"
     version   = "latest"
   }
 
---
+++ Azure/hello-world/output.tf
@@ -0,0 +1,3 @@
+output "endpoint" {
+  value = azurerm_public_ip.pip.fqdn
+}
